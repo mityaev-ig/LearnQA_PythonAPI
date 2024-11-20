585bfcc32efa90417f215d23eacb02c32bb9e8cf
mityaev-ig <mitaevi69@gmail.com>
2024-11-20 20:22:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-print("Hello from Mityaev Igor")
 import requests
 
-response =  requests.get("https://playground.learnqa.ru/api/get_text")
-print(response.text)
+response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
+redirect_count = len(response.history)
+last_response = response
+
+for resp in response.history:
+    print("URL -", resp.url, "код -", resp.status_code)
+
+print('Количество редиректов =', redirect_count)
+print("Конечный URL -", last_response.url, "код -", last_response.status_code)
+
+
+
