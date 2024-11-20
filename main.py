71531ab10f5d22293263b77255372a6f17a6c340
mityaev-ig <mitaevi69@gmail.com>
2024-11-20 23:21:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import requests
 
-response = requests.get("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
-redirect_count = len(response.history)
-last_response = response
-
-for resp in response.history:
-    print("URL -", resp.url, "код -", resp.status_code)
-
-print('Количество редиректов =', redirect_count)
-print("Конечный URL -", last_response.url, "код -", last_response.status_code)
-
-
-
+method_list = ["GET","POST","DELETE","PUT","HEAD","PATCH","OPTIONS"]
+zap_list = ["get","post","put","delete","head","patch", "options"]
+for zap in zap_list:
+    for meth in method_list:
+        method = {"method": meth}
+        if zap == "get":
+            response = requests.request(method=zap, url="https://playground.learnqa.ru/ajax/api/compare_query_type", params=method)
+        else:
+            response = requests.request(method=zap, url="https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
+        print("Запрос -", zap,",", "метод -", meth,"," "код -", response.status_code,"," "ответ -", response.text)
