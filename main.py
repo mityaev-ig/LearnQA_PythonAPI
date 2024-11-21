4a473b2345b6b5e697640b5c96dfa377506ec4e4
mityaev-ig <mitaevi69@gmail.com>
2024-11-21 04:10:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
+import time
 import requests
 
-method_list = ["GET","POST","DELETE","PUT","HEAD","PATCH","OPTIONS"]
-zap_list = ["get","post","put","delete","head","patch", "options"]
-for zap in zap_list:
-    for meth in method_list:
-        method = {"method": meth}
-        if zap == "get":
-            response = requests.request(method=zap, url="https://playground.learnqa.ru/ajax/api/compare_query_type", params=method)
+response1 = requests.get("https://playground.learnqa.ru/ajax/api/longtime_job")
+token_value = response1.json()
+token = token_value['token']
+seconds_value=response1.json()
+seconds = seconds_value['seconds']
+print(response1.text)
+
+response2 =requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params={'token':token})
+print(response2.text)
+status_value = response2.json()
+status = status_value['status']
+
+if status == "Job is NOT ready":
+    print("Задача еще не готова, статус верный")
+
+    time.sleep(seconds+1)
+
+    response3 =requests.get("https://playground.learnqa.ru/ajax/api/longtime_job", params={'token':token})
+    print(response3.text)
+    status_value = response3.json()
+    status = status_value['status']
+    result_value = response3.json()
+    result = result_value['result']
+
+    if status == "Job is ready":
+        print("Задача готова, статус верный")
+
+        if result != "":
+            print("Результат есть")
+
         else:
-            response = requests.request(method=zap, url="https://playground.learnqa.ru/ajax/api/compare_query_type", data=method)
-        print("Запрос -", zap,",", "метод -", meth,"," "код -", response.status_code,"," "ответ -", response.text)
+            print("Пустой результат")
+
+    else:
+        print("Статус неверный")
+
+else:
+    print("Статус неверный")
+
